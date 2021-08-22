6e2141a5da7c84fb34f1048bb938de2a1a6efc4d
Roy Janechuti <roysahn@hotmail.com>
2021-08-21 21:17:51 -0700

    categories object

--- app/views/includes/public_header.php
+++ app/views/includes/public_header.php
@@ -16,6 +16,5 @@
         <h1><?php echo SITE_NAME; ?></h1>
 
         <ul class="nav">
-            <li class="active"><a href="#">All items</a></li>
-            <li><a href="#">Category 1</a></li>
+            <?php $this->get_data('page_nav'); ?>
         </ul>

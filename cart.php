731a178854d83202c4acf67302e9a504358f9a92
Roy Janechuti <roysahn@hotmail.com>
2021-08-22 15:18:09 -0700

    cart CSS

--- cart.php
+++ cart.php
@@ -1,6 +1,6 @@
 <?php
 
 include('app/init.php');
-$Template->set_data('page_class', 'cart');
+$Template->set_data('page_class', 'shoppingcart');
 
 $Template->load('app/views/v_public_cart.php', 'Shopping Cart');
